c65c154d9ba20739adcfa1012d615300e331d7a5
Matthew Oertle <moertle@gmail.com>
2015-08-06 17:19:14 -0400

    remove pyaas references

--- _/py/settings.py
+++ _/py/settings.py
@@ -30,7 +30,7 @@ logging.basicConfig(
 
 
 def load():
-    parent = pyaas.util.getParent()
+    parent = _.util.getParent()
     script_name = os.path.basename(parent)
     script_name = script_name.rsplit('.', 1)[0]
 
@@ -44,52 +44,52 @@ def load():
             prefix = os.path.abspath(prefix)
 
     prefix = os.path.abspath(prefix)
-    if pyaas.prefix != prefix:
-        pyaas.prefix = prefix
-        logging.debug('Setting prefix to "%s"', pyaas.prefix)
+    if _.prefix != prefix:
+        _.prefix = prefix
+        logging.debug('Setting prefix to "%s"', _.prefix)
 
     if namespace is None:
         namespace = script_name
 
-    if namespace != pyaas.namespace:
-        pyaas.namespace = namespace
-        logging.debug('Setting namespace to "%s"', pyaas.namespace)
+    if namespace != _.namespace:
+        _.namespace = namespace
+        logging.debug('Setting namespace to "%s"', _.namespace)
 
     # if settings is not passed in use the supplied or derived namespace
     settings = settings or namespace
 
-    pyaas.args = pyaas.argparser.parse_args()
+    _.args = _.argparser.parse_args()
 
-    pyaas.config = configparser.SafeConfigParser(dict_type=collections.OrderedDict)
-    pyaas.config.optionxform = str
+    _.config = configparser.SafeConfigParser(dict_type=collections.OrderedDict)
+    _.config.optionxform = str
 
     ini_files = [
-        pyaas.paths('etc', settings + '.ini'),
-        pyaas.paths('etc', settings + '.ini.local')
+        _.paths('etc', settings + '.ini'),
+        _.paths('etc', settings + '.ini.local')
     ]
 
-    if pyaas.args.ini:
-        ini_files.append(pyaas.args.ini)
+    if _.args.ini:
+        ini_files.append(_.args.ini)
 
     try:
-        ok = pyaas.config.read(ini_files)
+        ok = _.config.read(ini_files)
     except configparser.ParsingError as e:
-        raise pyaas.error('Unable to parse file: %s', e)
+        raise _.error('Unable to parse file: %s', e)
 
     if not ok:
-        raise pyaas.error('Unable to read config file(s): %s', ini_files)
+        raise _.error('Unable to read config file(s): %s', ini_files)
 
     # setup file log
     file_name = '%s_%s.log' % (script_name, time.strftime('%Y%m%d_%H%M%S'))
 
     # hack back in single log file option without breaking existing code
-    if pyaas.config.has_section('logging'):
-        if pyaas.config.has_option('logging', 'append'):
-            append = pyaas.config.getboolean('logging', 'append')
+    if _.config.has_section('logging'):
+        if _.config.has_option('logging', 'append'):
+            append = _.config.getboolean('logging', 'append')
             if append:
                 file_name = script_name + '.log'
 
-    full_path = pyaas.paths('var', file_name)
+    full_path = _.paths('var', file_name)
     logfile = logging.FileHandler(full_path)
     logfile.setLevel(logging.INFO)
 
@@ -104,12 +104,12 @@ def load():
     root = logging.getLogger()
     root.addHandler(logfile)
 
-    if pyaas.args.debug:
+    if _.args.debug:
         root.setLevel(logging.DEBUG)
         logfile.setLevel(logging.DEBUG)
 
     # call this here if there is no daemon option
-    if not hasattr(pyaas.args, 'daemon'):
-        pyaas.module.load()
+    if not hasattr(_.args, 'daemon'):
+        _.module.load()
 
     return
