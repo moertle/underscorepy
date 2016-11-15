c8b73a03b234284cea50bbf21251c2ceec25bf70
Matthew Oertle <moertle@gmail.com>
2016-11-15 13:12:15 -0500

    colors

--- _/py/io.py
+++ _/py/io.py
@@ -3,30 +3,38 @@ import sys
 
 import _
 
-out = sys.stdout
+from . import colors
 
 
-class Colors:
-    def black  (self, fmt='', *args): self('\x1b[1;30m' + fmt + '\x1b[0m', *args)
-    def red    (self, fmt='', *args): self('\x1b[1;31m' + fmt + '\x1b[0m', *args)
-    def green  (self, fmt='', *args): self('\x1b[1;32m' + fmt + '\x1b[0m', *args)
-    def yellow (self, fmt='', *args): self('\x1b[1;33m' + fmt + '\x1b[0m', *args)
-    def blue   (self, fmt='', *args): self('\x1b[1;34m' + fmt + '\x1b[0m', *args)
-    def purple (self, fmt='', *args): self('\x1b[1;35m' + fmt + '\x1b[0m', *args)
-    def cyan   (self, fmt='', *args): self('\x1b[1;36m' + fmt + '\x1b[0m', *args)
-    def white  (self, fmt='', *args): self('\x1b[1;37m' + fmt + '\x1b[0m', *args)
+class Printf(object):
+    def __init__(self):
+        self.out = sys.stdout
+        self._colors = colors.xterm256
 
-
-class Printf(Colors):
     def __call__(self, fmt='', *args):
-        out.write(fmt % args)
-        out.flush()
+        self.out.write(fmt % args)
+        self.out.flush()
+        return self
+
+    def __getitem__(self, name):
+        self.out.write(getattr(self._colors, name))
+        return self
+
+    def __getattr__(self, name):
+        self.out.write(getattr(self._colors, name))
+        return self
+
+    def colors(self, name):
+        if not hasattr(colors, name):
+            raise ValueError
+        self._colors = getattr(colors, name)
+        return self
 
 
-class Writeln(Colors):
+class Writeln(Printf):
     def __call__(self, fmt='', *args):
-        out.write(fmt % args)
-        out.write('\n')
+        fmt += self._colors.endl
+        return super(Writeln, self).__call__(fmt, *args)
 
 
 def hexdump(blob, width=16, offset=0):
@@ -35,15 +43,15 @@ def hexdump(blob, width=16, offset=0):
         line = blob[:width]
         blob = blob[width:]
 
-        _.printf.white(fmt, offset)
-        _.printf.cyan(' '.join('%.2x' % ord(c) for c in line))
+        _.printf.WHITE(fmt, offset)
+        _.printf.CYAN.bold(' '.join('%.2x' % ord(c) for c in line))
         _.printf(' ' * ((width-len(line))*3+1))
 
         for c in line:
             if ord(c) < 32 or ord(c) > 126:
-                _.printf.black('.')
+                _.printf.VIOLET('.')
             else:
-                _.printf.white('%c', c)
+                _.printf.WHITE.bold('%c', c)
 
         _.printf('\n')
         offset += width
