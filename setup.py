6e19382fa941c7c93903c92bd7e6f145402480b2
Matthew Oertle <moertle@gmail.com>
2017-02-11 03:59:28 -0500

    version bump

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3.x',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         ]
     )
