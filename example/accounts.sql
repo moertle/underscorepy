ebe947294b7b4df2d2168982b40f2293ef119246
Matthew Shaw <mshaw.cx@gmail.com>
2023-03-07 18:27:46 -0500

    skeleton has become the example

--- example/accounts.sql
+++
@@ -1,9 +0,0 @@
-
--- _.py users
-CREATE TABLE IF NOT EXISTS users (
-    "username"      TEXT        UNIQUE NOT NULL,
-    "password"      TEXT        NOT NULL,
-    "disabled"      BOOLEAN,
-    "created"       INTEGER,
-    "last"          INTEGER
-    );
