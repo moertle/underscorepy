31f6885952abfe0a92048ecd1df110b37a729180
Matthew Shaw <mshaw.cx@gmail.com>
2023-03-09 21:27:43 -0500

    condense websockets

--- skeleton/skeleton/handlers.py
+++ skeleton/skeleton/handlers.py
@@ -1,9 +1,9 @@
 
-import _.handlers.websockets
+import _.websockets
 import tornado.web
 
 
-class Socket(_.handlers.websockets.Protected):
+class Socket(_.websockets.Protected):
     def on_message(self, msg):
         logging.info('websocket: %s', msg)
         self.write_message(msg)
